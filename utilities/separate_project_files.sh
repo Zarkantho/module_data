1bbed9f7d25472239e3714afcd7b187e923397fe
Shaun Verch <shaun.verch@10gen.com>
2014-01-09 15:35:43 -0800

    Generated third party module text

--- utilities/separate_project_files.sh
+++ utilities/separate_project_files.sh
@@ -2,12 +2,14 @@
 
 DIR_NAME=""
 mkdir mongodb
+rm mongodb/general_notes.txt
 while read line
 do
     case $line in
         \#\#\#\#\#*)
             DIR_NAME=`echo $line | sed 's/#* \(.*\) #*/\1/'`
             mkdir mongodb/$DIR_NAME
+            rm mongodb/$DIR_NAME/module.txt
             ;;
     esac
     if [ -z $DIR_NAME ]
@@ -16,4 +18,4 @@ do
     else
         echo $line >> mongodb/$DIR_NAME/module.txt
     fi
-done < "project_files.txt"
+done < "utilities/project_files.txt"
