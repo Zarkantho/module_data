202f4b1b9e7fb39bcbf6862866314549723009ed
Shaun Verch <shaun.verch@10gen.com>
2014-01-20 12:57:17 -0800

    Added yaml output for module files

--- utilities/process_module_files.py
+++ utilities/process_module_files.py
@@ -4,19 +4,12 @@ import sys
 import os
 import re
 import json
+import yaml
 
-def main():
-
-    if len(sys.argv) != 3:
-        print("Usage: process_module_files.py <modules_directory> <modules_description_file>")
-        sys.exit(1)
-
+def split_txt_version(modules_directory, modules_description_filename):
     title_regex = re.compile("^#+ (.*) #+")
     comment_regex = re.compile("^(#+.*)")
     source_file_regex = re.compile("((:?(:?src)|(:?build))\S*)(:?.*)?")
-
-    modules_directory = sys.argv[1]
-    modules_description_filename = sys.argv[2]
 
     result_map = {}
     current_comments = ""
@@ -80,7 +73,38 @@ def main():
         module_json_file = open(os.path.join(modules_directory, module_name, 'module.json'), 'w')
         module_json_file.write(json.dumps(result_map[module_name], indent=4, separators=(',', ': ')))
 
-    with open(os.path.join(modules_directory, 'modules.json'), 'w') as f:
-        f.write(json.dumps(result_map, indent=4, separators=(',', ': ')))
+    return result_map
+
+def split_yaml_version(modules_directory, modules_description_filename):
+
+    modules_description_file = open(modules_description_filename)
+
+    result_map = yaml.load(modules_description_file)
+
+    for module_name in result_map.keys():
+        module_json_file = open(os.path.join(modules_directory, module_name, 'module.json'), 'w')
+        module_json_file.write(json.dumps(result_map[module_name], indent=4, separators=(',', ': ')))
+
+    return result_map
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Usage: process_module_files.py <modules_directory> <modules_description_file>")
+        sys.exit(1)
+
+    modules_directory = sys.argv[1]
+    modules_description_filename = sys.argv[2]
+
+    if modules_description_filename.endswith(".yaml"):
+        with open(os.path.join(modules_directory, 'modules.json'), 'w') as f:
+            f.write(json.dumps(split_yaml_version(modules_directory, modules_description_filename), indent=4, separators=(',', ': ')))
+        with open(os.path.join(modules_directory, 'modules.yaml'), 'w') as f:
+            f.write(json.dumps(split_yaml_version(modules_directory, modules_description_filename), indent=4, separators=(',', ': ')))
+    else:
+        with open(os.path.join(modules_directory, 'modules.json'), 'w') as f:
+            f.write(json.dumps(split_txt_version(modules_directory, modules_description_filename), indent=4, separators=(',', ': ')))
+        with open(os.path.join(modules_directory, 'modules.yaml'), 'w') as f:
+            f.write(json.dumps(split_txt_version(modules_directory, modules_description_filename), indent=4, separators=(',', ': ')))
 
 main()
